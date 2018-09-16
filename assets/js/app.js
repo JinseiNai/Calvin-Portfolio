424fdd13f4beb9ff79506c3ee1510d52b39afead
Calvin <jinseinai@gmail.com>
2018-09-15 22:54:18 -0700

    finito

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,38 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(elems, options);
-// });
-
-// Or with jQuery
-
 $(document).ready(function () {
-    $('.carousel').carousel();
+    $("#about-well, #project-well, #contact-well").hide();
+    $("#about-well").show();
+
+    $('.slider').slider();
+
+    $("#nav-btn").on("click", function(event) {
+        let target = event.target;
+        console.log(target.text);
+        hideAll();
+
+        switch (target.text) {
+            case "About":
+            $("#about-well").slideDown(1000).show();
+            break;
+
+            case "Project":
+            $("#project-well").slideDown(1000).show();
+            break;
+
+            case "Contact":
+            $("#contact-well").slideDown(1000).show();
+            break;
+
+            default:
+            $("#about-well").slideDown(1000).show();
+        }
+    });
+
+    $("#submit").on("click", function() {
+        $("#firstName, #lastName, #email, #message").val("");
+        alert("Your message has been sent. Thank You!");
+    })
 });
-// $('.carousel').carousel()
+
+function hideAll() {
+    $("#about-well, #project-well, #contact-well").slideUp(1000).hide();
+}
